46da9fb98b86b28c963cf5007325355a3e23d37a
CREUTZBURG Brian <bcreutz@deq.state.or.us>
2018-02-06 15:21:31 -0800

    getting closer. still problem with layerfile in line 18

--- WatershedMap.py
+++ WatershedMap.py
@@ -1,28 +1,26 @@
 import arcpy
 import os
 
-# base folder
+
 str_dir_main = r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps"
 str_mxd_blank = r"Upper_Yaquina_TMDL_Blank_ME.mxd"
 str_df_cur = r"Layers"
 str_lyr_cur = r"UY_*.lyr"
 str_mxd_save_pre = r"Upper_Yaquina_TMDL_UY_"
 str_mxd_save_post = r".mxd"
+lyr_mxd_save_post = r".lyr"
 
 mxd = arcpy.mapping.MapDocument(str_dir_main + "\\" + str_mxd_blank)
 df = arcpy.mapping.ListDataFrames(mxd, str_df_cur)[0]
-lyrList =
-walk = arcpy.da.Walk(str_dir_main, datatype="Layer")
+lyrList = ["Elev", "Precip", "Geo"]
 
-for dirpath, dirnames, filenames in walk:
-    for filename in filenames:
-        layerfile = os.path.join(dirpath, filename)
-        addlayer = arcpy.mapping.Layer(layerfile)
-        arcpy.mapping.AddLayer(df, addlayer, "BOTTOM")
-        arcpy.RefreshTOC()
-        arcpy.RefreshActiveView()
-        mxd.saveACopy(str_dir_main + "\\" + str_mxd_save_pre + str_mxd_save_post)
-
+for lyrName in lyrList:
+    layerfile = (str_dir_main + "\\" + lyrName + lyr_mxd_save_post)
+    addlayer = arcpy.mapping.Layer(layerfile)
+    arcpy.mapping.AddLayer(df, addlayer, "BOTTOM")
+    arcpy.RefreshTOC()
+    arcpy.RefreshActiveView()
+    mxd.saveACopy(str_dir_main + "\\" + str_mxd_save_pre + LyrName + str_mxd_save_post)
 
 
 del addlayer, mxd, df
