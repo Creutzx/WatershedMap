b9f19d61be188b7bbc0436f043ec1612fe7addf0
CREUTZBURG Brian <bcreutz@deq.state.or.us>
2018-02-06 15:50:35 -0800

    getting error in line 19 still'

--- WatershedMap.py
+++ WatershedMap.py
@@ -8,14 +8,14 @@ str_df_cur = r"Layers"
 str_lyr_cur = r"UY_*.lyr"
 str_mxd_save_pre = r"Upper_Yaquina_TMDL_UY_"
 str_mxd_save_post = r".mxd"
-lyr_mxd_save_post = r".lyr"
+lyr_save_post = r".lyr"
 
 mxd = arcpy.mapping.MapDocument(str_dir_main + "\\" + str_mxd_blank)
 df = arcpy.mapping.ListDataFrames(mxd, str_df_cur)[0]
 lyrList = ["Elev", "Precip", "Geo"]
 
 for lyrName in lyrList:
-    layerfile = (str_dir_main + "\\" + lyrName + lyr_mxd_save_post)
+    layerfile = os.path.join(str_dir_main, lyrName + lyr_save_post)
     addlayer = arcpy.mapping.Layer(layerfile)
     arcpy.mapping.AddLayer(df, addlayer, "BOTTOM")
     arcpy.RefreshTOC()
